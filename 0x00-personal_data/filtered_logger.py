9fa1b087a97205050d0f443ac77af7d0dd511d77
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-02 18:41:07 +0100

    Connect to secure database/ Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,8 @@
 import logging
 import re
 from typing import List
-import mysql.connector
-import os
+from mysql.connector import connection
+from os import environ
 
 
 PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
@@ -32,6 +32,22 @@ def get_logger() -> logging.Logger:
     return logger
 
 
+def get_db() -> connection.MySQLConnection:
+    """
+    Connect to mysql server with environmental vars: Task 3
+    """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+    connector = connection.MySQLConnection(
+        user=username,
+        password=password,
+        host=db_host,
+        database=db_name)
+    return connector
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class for Task- 1"""
 
@@ -52,35 +68,28 @@ class RedactingFormatter(logging.Formatter):
             self.SEPARATOR)
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    Connect to mysql server with environmental vars: Task 3
-    """
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
-    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
-    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
-                                   password=passwd,
-                                   host=host,
-                                   database=db_name)
-    return conn
-
-
-def main():
+def main() -> None:
     """
     Obtain a database connection using get_db
-    and retrieve all rows in the users table and display each row
+    and retrieve all rows in the users table and display each row: Task 4
     """
     db = get_db()
+    cur = db.cursor()
+
+    query = ('SELECT * FROM users;')
+    cur.execute(query)
+    fetch_data = cur.fetchall()
+
     logger = get_logger()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    fields = cursor.column_names
-    for row in cursor:
-        message = "".join("{}={}; ".format(k, v) for k, v in zip(fields, row))
-        logger.info(message.strip())
-    cursor.close()
+
+    for row in fetch_data:
+        fields = 'name={}; email={}; phone={}; ssn={}; password={}; ip={}; '\
+            'last_login={}; user_agent={};'
+        fields = fields.format(row[0], row[1], row[2], row[3],
+                               row[4], row[5], row[6], row[7])
+        logger.info(fields)
+
+    cur.close()
     db.close()
 
 
