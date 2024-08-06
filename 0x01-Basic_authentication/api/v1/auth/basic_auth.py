9bfb685d5744e4f8912854b4b979136fb9e7aaa3
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-06 23:11:57 +0100

    Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,69 +1,55 @@
 #!/usr/bin/env python3
-"""
-Definition of class BasicAuth
-"""
-import base64
-from .auth import Auth
-from typing import TypeVar
+"""Basic API authentication module."""
 
+from api.v1.auth.auth import Auth
+from base64 import b64decode
 from models.user import User
+from typing import TypeVar, Tuple
+
+
+User = TypeVar('User')
 
 
 class BasicAuth(Auth):
-    """ Implement Basic Authorization protocol methods
-    """
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
-        """
-        Extracts the Base64 part of the Authorization header for a Basic
-        Authorization
-        """
-        if authorization_header is None:
-            return None
-        if not isinstance(authorization_header, str):
-            return None
-        if not authorization_header.startswith("Basic "):
-            return None
-        token = authorization_header.split(" ")[-1]
-        return token
+    """Basic Authentication. Task 6"""
 
-    def decode_base64_authorization_header(self,
-                                           base64_authorization_header:
-                                           str) -> str:
-        """
-        Decode a Base64-encoded string
-        """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Returns Base64 part of Authorization header. Task 7"""
+        if authorization_header and isinstance(
+                authorization_header,
+                str) and authorization_header.startswith("Basic "):
+            return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns decoded value of base64_authorization_header. Task 8"""
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
             return None
+
         try:
-            decoded = base64_authorization_header.encode('utf-8')
-            decoded = base64.b64decode(decoded)
-            return decoded.decode('utf-8')
+            return b64decode(base64_authorization_header).decode('utf-8')
         except Exception:
             return None
 
-    def extract_user_credentials(self,
-                                 decoded_base64_authorization_header:
-                                 str) -> (str, str):
-        """
-        Returns user email and password from Base64 decoded value
-        """
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """Returns user email and pswd from decoded Base64.Task 9 & 12(":")"""
         if decoded_base64_authorization_header is None:
-            return (None, None)
+            return None, None
         if not isinstance(decoded_base64_authorization_header, str):
-            return (None, None)
-        if ':' not in decoded_base64_authorization_header:
-            return (None, None)
-        email = decoded_base64_authorization_header.split(":")[0]
-        password = decoded_base64_authorization_header[len(email) + 1:]
-        return (email, password)
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        email, pwd = decoded_base64_authorization_header.split(':', 1)
+        return (email, pwd)
 
     def user_object_from_credentials(self, user_email: str,
-                                     user_pwd: str) -> TypeVar('User'):
+                                     user_pwd: str) -> User:
         """
-        Return a User instance based on email and password
+        Return a User instance based on email and password. Task 10
         """
         if user_email is None or not isinstance(user_email, str):
             return None
@@ -79,6 +65,7 @@ class BasicAuth(Auth):
             return None
         except Exception:
             return None
+
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
