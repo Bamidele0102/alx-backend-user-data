fa52d0c8c7a4de4d59743a143d706a9a171f5fe6
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-06 23:31:10 +0100

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,28 +56,10 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         try:
-            users = User.search({"email": user_email})
-            if not users or users == []:
-                return None
-            for u in users:
-                if u.is_valid_password(user_pwd):
-                    return u
-            return None
+            user = User.search({'email': user_email})
         except Exception:
             return None
-
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Returns a User instance based on a received request
-        """
-        Auth_header = self.authorization_header(request)
-        if Auth_header is not None:
-            token = self.extract_base64_authorization_header(Auth_header)
-            if token is not None:
-                decoded = self.decode_base64_authorization_header(token)
-                if decoded is not None:
-                    email, pword = self.extract_user_credentials(decoded)
-                    if email is not None:
-                        return self.user_object_from_credentials(email, pword)
-        return
+        for u in user:
+            if u.is_valid_password(user_pwd):
+                return u
+        return None
