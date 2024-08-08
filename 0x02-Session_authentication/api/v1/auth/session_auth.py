6d25d4d40d69123c8e266f4419f322a50c4109e4
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-08 23:52:10 +0100

    Bug fixes

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,61 +1,71 @@
 #!/usr/bin/env python3
-""" Module of Session Auth
 """
-from api.v1.views import app_views
-from flask import abort, jsonify, request
+Definition of class SessionAuth
+"""
+import base64
+from uuid import uuid4
+from typing import TypeVar
+
+from .auth import Auth
 from models.user import User
-from os import getenv
 
 
-@app_views.route('/auth_session/logout',
-                 methods=['DELETE'],
-                 strict_slashes=False)
-def session_logout() -> str:
-    """ DELETE /api/v1/auth_session/logout
-    JSON body:
-    - session id
-    Return:
-      - Empty JSON
+class SessionAuth(Auth):
+    """ Implement Session Authorization protocol methods
     """
-    from api.v1.app import auth
+    user_id_by_session_id = {}
 
-    logout = auth.destroy_session(request)
-    if not logout:
-        abort(404)
-    return jsonify({}), 200
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user with id user_id
+        Args:
+            user_id (str): user's user id
+        Return:
+            None is user_id is None or not a string
+            Session ID in string format
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
 
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a user ID based on a session ID
+        Args:
+            session_id (str): session ID
+        Return:
+            user id or None if session_id is None or not a string
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_login() -> str:
-    """ POST /api/v1/auth_session/login
-    JSON body:
-      - email
-      - password
-    Return:
-      - User object JSON represented
-    """
+    def current_user(self, request=None):
+        """
+        Return a user instance based on a cookie value
+        Args:
+            request : request object containing cookie
+        Return:
+            User instance
+        """
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        user = User.get(user_id)
+        return user
 
-    user_email = request.form.get('email')
-    user_pswd = request.form.get('password')
-
-    if not user_email:
-        return jsonify({"error": "email missing"}), 400
-    if not user_pswd:
-        return jsonify({"error": "password missing"}), 400
-
-    try:
-        search_users = User.search({'email': user_email})
-    except Exception:
-        return jsonify({"error": "no user found for this email"}), 404
-    if not search_users:
-        return jsonify({"error": "no user found for this email"}), 404
-
-    user = search_users[0]
-    if not user.is_valid_password(user_pswd):
-        return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    session_cookie = getenv("SESSION_NAME")
-    session_id = auth.create_session(user.id)
-    response = jsonify(user.to_json())
-    response.set_cookie(session_cookie, session_id)
-    return response
+    def destroy_session(self, request=None):
+        """
+        Deletes a user session
+        """
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True
